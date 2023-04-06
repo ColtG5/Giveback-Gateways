2bbba3f386f5aaae4a0213cd5e8c7082d94971d5
ColtG5 <colton.gowans@ucalgary.ca>
2023-04-06 14:32:38 -0600

    Update main.tsx

--- giveback-gateways/src/main.tsx
+++ giveback-gateways/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraBaseProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <App />
-    </ChakraBaseProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
