7f2c3a0ca5f4b60db2ff18506c141f20d7ae920e
Habiba Abuelazm <habiba.abuelazm1@gmail.com>
2023-04-11 00:08:29 -0600

    Adding data to db for testing purposes

--- database471.sql
+++ database471.sql
@@ -6,28 +6,11 @@ DROP DATABASE IF EXISTS 'gbgw471';
 CREATE DATABASE 'gbgw471';
 USE 'gbgw471';
 
-CREATE TABLE 'Client' (
-    'clientID' INT AUTO_INCREMENT NOT NULL,
-    PRIMARY KEY ('clientID')
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-CREATE TABLE 'Volunteer_user' (
-    'vID' INT NOT NULL,
-    PRIMARY KEY ('vID'),
-    FOREIGN KEY ('vID') REFERENCES 'Client' ('clientID') ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-CREATE TABLE 'Company_user' (
-    'cID' INT NOT NULL,
-    PRIMARY KEY ('cID'),
-    FOREIGN KEY ('cID') REFERENCES 'Client' ('clientID') ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
 CREATE TABLE 'Profile' (
     'Username' varchar(25) NOT NULL,
     'Password' varchar(25) NOT NULL,
-    'FirstName' varchar(25) NOT NULL,
-    'LastName' varchar(25) NOT NULL,
+    'Name' varchar(25) NOT NULL,
+    'LastName' varchar(25) DEFAULT NULL,
     'Email' varchar(256) NOT NULL,
     'Phone' char(10) DEFAULT NULL,
     'Biography' varchar(150) DEFAULT NULL,
@@ -36,22 +19,32 @@ CREATE TABLE 'Profile' (
     PRIMARY KEY ('Username')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO 'Profile' ('Username', 'Password', 'Name', 'LastName', 'Email', 'Phone', 'Biography', 'Location', 'CreationDate') VALUES
+('ColtG5', 'polarbear123', 'Colton', 'Gowans', 'coltongowans@gmail.com', '1234567890', 'POLAR BEARS 4 LIFE', 'Calgary', '2023-04-10'),
+('gbgw123', 'lovegivingback321', 'GiveBack GateWays', '', 'givebackggw@gmail.com', '7653451234', 'We are all about giving back', 'Calgary', '2023-04-11'),
+('xlishx', 'databasesrock', 'Alisha', 'Nasir', 'alisha.nasir456@gmail.com', '9876543210', 'I LOVE DATABASES', 'Calgary', '2023-04-10');
+
 CREATE TABLE 'Volunteer_profile' (
+    'vID' INT AUTO_INCREMENT NOT NULL,
     'vUser' varchar(25) NOT NULL,
-    'vID' INT NOT NULL,
     'Hours' INT DEFAULT NULL,
-    PRIMARY KEY ('vUser', 'vID'),
-    FOREIGN KEY ('vUser') REFERENCES 'Profile' ('Username') ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY ('vID') REFERENCES 'Volunteer_user' ('vID') ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY ('vID'),
+    FOREIGN KEY ('vUser') REFERENCES 'Profile' ('Username') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO 'Volunteer_profile' ('vUser', 'Hours') VALUES
+('ColtG5', 0),
+('xlishx', 100);
+
 CREATE TABLE 'Company_profile' (
+    'cID' INT AUTO_INCREMENT NOT NULL,
     'cUser' varchar(25) NOT NULL,
-    'cID' INT NOT NULL,
-    PRIMARY KEY ('cUser', 'cID'),
-    FOREIGN KEY ('cUser') REFERENCES 'Profile' ('Username') ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY ('cID') REFERENCES 'Volunteer_user' ('cID') ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY ('cID'),
+    FOREIGN KEY ('cUser') REFERENCES 'Profile' ('Username') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO 'Company_profile' ('cUser') VALUES
+('gbgw123');
 
 CREATE TABLE 'User_goals' (
     'vUser' varchar(25) NOT NULL,
@@ -75,6 +68,10 @@ CREATE TABLE 'Profile_follows' (
     FOREIGN KEY ('cUser') REFERENCES 'Company_profile' ('cUser') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO 'Profile_follows' ('vUser', 'cUser') VALUES
+('ColtG5', 'gbgw123'),
+('xlishx', 'gbgw123');
+
 CREATE TABLE 'Volunteering_opportunity' (
     'ID' INT AUTO_INCREMENT NOT NULL,
     'Title' varchar(25) NOT NULL,
@@ -88,6 +85,10 @@ CREATE TABLE 'Volunteering_opportunity' (
     FOREIGN KEY ('cUser') REFERENCES 'Company_profile' ('cUser') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO 'Volunteering_opportunity' ('Title', 'Date', 'Time', 'Duration', 'Description', 'VolunteersNeeded', 'cUser') VALUES
+('Beach Cleanup', '2023-04-17', '16:00', 2, 'Help us make our citys beaches look squeaky clean', 10, 'gbgw123'),
+('Orchestra hosts', '2023-04-24', '18:00', 2, 'Our citys national yearly orchestra!', 5, 'gbgw123');
+
 CREATE TABLE 'SignedUp_Opportunities' (
     'vUser' varchar(25) NOT NULL,
     'OppID' INT NOT NULL,
@@ -97,8 +98,12 @@ CREATE TABLE 'SignedUp_Opportunities' (
     'Attended' tinyint(1) DEFAULT 0,
     PRIMARY KEY ('vUser', 'OppID'),
     FOREIGN KEY ('vUser') REFERENCES 'Volunteer_profile' ('vUser') ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY ('OppID') REFERENCES 'Volunteering_opportunity' ('cUser') ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY ('OppID') REFERENCES 'Volunteering_opportunity' ('ID') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO 'SignedUp_Opportunities' ('vUser', 'OppID', 'Accepted', 'Rejected', 'Pending', 'Attended') VALUES
+('ColtG5', 2, 0, 0, 1, 0),
+('xlishx', 1, 1, 0, 0, 0);
 
 CREATE TABLE 'Message_board' (
     'boardID' INT AUTO_INCREMENT PRIMARY KEY,
@@ -107,25 +112,36 @@ CREATE TABLE 'Message_board' (
     FOREIGN KEY ('cUser') REFERENCES 'Company_profile' ('cUser') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO 'Message_board' ('cUser') VALUES
+('gbgw123');
+
 CREATE TABLE 'Message' (
     'messageID' INT AUTO_INCREMENT NOT NULL,
-    'vUser' varchar(25) NOT NULL,
+    'cUser' varchar(25) NOT NULL,
     'bID' INT NOT NULL,
     'Title' varchar(25) NOT NULL,
     'Content' varchar(255) NOT NULL,
     'Date' date NOT NULL,
     'Time' time NOT NULL,
     PRIMARY KEY ('messageID'),
-    FOREIGN KEY ('vUser') REFERENCES 'Volunteer_profile' ('vUser') ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ('vUser') REFERENCES 'Company_profile' ('cUser') ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY ('bID') REFERENCES 'Message_board' ('boardID') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+INSERT INTO 'Message' ('cUser', 'bID', 'Title', 'Content', 'Date', 'Time') VALUES
+('gbgw123', 1, 'Important Update', 'Time of cleanup has changed!', '2023-04-10'),
+('gbgw123', 1, 'Change of plans', 'We have changed the location', '2023-04-10');
+
 CREATE TABLE 'Board_follows' (
-    'cUser' varchar(25) NOT NULL,
+    'vUser' varchar(25) NOT NULL,
     'boardID' INT NOT NULL,
-    PRIMARY KEY ('cUser', 'boardID'),
-    FOREIGN KEY ('cUser') REFERENCES 'Company_profile' ('cUser') ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY ('vUser', 'boardID'),
+    FOREIGN KEY ('vUser') REFERENCES 'Volunteer_profile' ('vUser') ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY ('boardID') REFERENCES 'Message_board' ('boardID') ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO 'Board_follows' ('vUser', 'boardID') VALUES
+('ColtG5', 1),
+('xlishx', 1);
 
 COMMIT;
