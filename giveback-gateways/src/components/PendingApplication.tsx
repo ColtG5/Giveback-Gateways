238f8223b1bf2562f87dd1779d3c82648f368193
Habiba Abuelazm <habiba.abuelazm1@gmail.com>
2023-04-14 13:48:58 -0600

    Fixing everythinggggg

--- giveback-gateways/src/components/PendingApplication.tsx
+++ giveback-gateways/src/components/PendingApplication.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import { Box, Text, HStack } from '@chakra-ui/react';
 import { PendingApplicationProps } from '../services/pending-application-service';
 
-const PendingApplication = ({ volunteerName, location, email, phoneNumber, opportunityTitle, children }: PendingApplicationProps) => (
+const PendingApplication = ({ location, email, phoneNumber, opportunityTitle, children }: PendingApplicationProps) => (
   <Box borderWidth="1px" borderRadius="lg" p={4} mb={4}>
     <HStack justifyContent="space-between">
       <Box>
-        <Text fontWeight="bold">{volunteerName}</Text>
+        <Text fontWeight="bold">{email}</Text>
         <Text>Location: {location}</Text>
-        <Text>Email: {email}</Text>
         <Text>Phone: {phoneNumber}</Text>
         <Text>Opportunity: {opportunityTitle}</Text>
       </Box>
