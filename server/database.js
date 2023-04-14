1ab4eb1a5965428fbf5a230ed47213399592cfd1
alisha-nas <al.alishanasir@gmail.com>
2023-04-14 14:07:15 -0600

    password change

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ const {createPool} = require('mysql2')
 const pool = createPool ({ 
   host: "localhost",
   user: "root",
-  password: "habiba471",
+  password: "alisha.nasir.471",
   connecLimit: 10 
 })
 
