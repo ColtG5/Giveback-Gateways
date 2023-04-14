4ee50e4ef6f032ef81ee98586d86ece890df1fe5
Habiba Abuelazm <100043454+habiba-abuelazm@users.noreply.github.com>
2023-04-14 12:33:20 -0600

    Update database.js

--- server/database.js
+++ server/database.js
@@ -159,6 +159,21 @@ const retrieveMessages = (bID, callback) => {
 };
 
 const retrieveProfileInfo = (username, callback) => {
+  const query = 'SELECT * FROM gbgw471.Profile WHERE Username = ?';
+  pool.query(query, [username], (err, results) => {
+    if (err) {
+      // Handle error
+      console.error('Failed to retrieve profile information:', err);
+      callback(err, null);
+    } else {
+      // Send the retrieved data back to the callback function
+      console.log(results)
+      callback(null, results);
+    }
+  });
+};
+
+const retrieveVolunteerName = (username, callback) => {
   const query = 'SELECT Name FROM gbgw471.Profile WHERE Username = ?';
   pool.query(query, [username], (err, results) => {
     if (err) {
@@ -200,7 +215,7 @@ const deleteSignedOpportunity = (vUser, OppID) => {
 };
 
 const retrieveSignedUpOpportunities = (username, callback) => {
-  const query = 'SELECT * FROM gbgw471.SignedUp_Opportunities WHERE vUser = ?';
+  const query = 'SELECT Volunteering_opportunity.Title, Volunteering_opportunity.Date, Volunteering_opportunity.Time, Volunteering_opportunity.Duration, Volunteering_opportunity.Description FROM gbgw471.SignedUp_Opportunities, gbgw471.Volunteering_opportunity WHERE SignedUp_Opportunities.OppID = Volunteering_opportunity.ID AND vUser = ?';
   pool.query(query, [username], (err, results) => {
     if (err) {
       // Handle error
